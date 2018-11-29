af0def1c9bb44f9e5279f856ad870b0a70f6bc30
zhitou25 <2671184532@qq.com>
2018-11-29 20:57:06 +0800

    添加二叉树相关算法

--- Data Structure/BiTree.c
+++ Data Structure/BiTree.c
@@ -49,6 +49,44 @@ void PostOrderTraverse(BiTree T)//后序遍历
 	printf("%c ", T->val);
 }
 
+//获取二叉树的高度
+int BiTreeDepth(BiTree bt)
+{
+	if (!bt)
+		return 0;
+	return BiTreeDepth(bt->lchild) > BiTreeDepth(bt->rchild) ? BiTreeDepth(bt->lchild) + 1 : BiTreeDepth(bt->rchild) + 1;
+}
+//获取二叉树第k层的结点个数
+int  BiTreeFlowNum(BiTree bt, int k)
+{
+	if (NULL == bt || k <1)
+		return 0;
+	if (k == 1)
+		return 1;
+	return BiTreeFlowNum(bt->lchild, k - 1) + BiTreeFlowNum(bt->rchild, k - 1);
+}
+//求二叉树的节点数
+int BiTreeNode(BiTree bt)
+{
+	if (bt == NULL)
+		return 0;
+	return BiTreeNode(bt->lchild) + BiTreeNode(bt->rchild) + 1;
+}
+//返回二叉树叶子节点的个数
+int BiTreeLeafNode(BiTree bt)
+{
+	//int count=0;
+	if (!bt)
+		return 0;
+	if ((BiTreeLeafNode(bt->lchild) == 0 && BiTreeLeafNode(bt->rchild) == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return BiTreeLeafNode(bt->lchild) + BiTreeLeafNode(bt->rchild);
+	}
+}
 
 
 
@@ -66,6 +104,10 @@ int main()
 	printf("\n");
 	PostOrderTraverse(T);
 	printf("\n");
+	printf("二叉树总共有%d个节点\n", BiTreeNode(T));
+	printf("树的高度是%d\n", BiTreeDepth(T));
+	printf("有%d个叶子节点\n", BiTreeLeafNode(T));
+	printf("第三层有%d个节点\n", BiTreeFlowNum(T, 3));
 	system("pause");
 	return 0;
 }
